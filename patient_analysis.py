93c881f958867da16cfd63161ca5d95272854ceb
wafiakmal <wafiakmal@gmail.com>
2023-03-02 21:09:48 -0500

    update

--- patient_analysis.py
+++ patient_analysis.py
@@ -11,10 +11,10 @@ def process_file(filepath: str) -> list[dict[str, str]]:
     Time complexity analysis:
     An empty list is created for holding data O(1).
     The file based on filepath is opened with utf-8-sig encoding O(1).
-    A key created for the header of the file with O(1) operation.
+    A key created for the header of the file that scales with number of column
+    size M times.
     Using for loop, each line in the file is read with N times.
-    Creating a dictionary from the header row and data row with a linear time
-    operation with respect to the number of columns in the file M times.
+    A dictionary is then created based on the value of each line in the file.
     The value added to dictionary is performed with 3 times O(1), for N times.
     The dictionary is then appended to a list O(1), which is returned O(1).
     Overall time complexity is O(N*M)
@@ -24,9 +24,9 @@ def process_file(filepath: str) -> list[dict[str, str]]:
     """
     hold_data = []  # O(1)
     with open(filepath, encoding="utf-8-sig") as f:  # O(1)
-        header = f.readline().strip().split("\t")  # O(1)
+        header = f.readline().strip().split("\t")  # M times
         for line in f:  # N times
-            values = line.strip().split("\t")  # M times
+            values = line.strip().split("\t")  #  O(1)
             row = dict(zip(header, values))  # O(1)
             hold_data.append(row)  # O(1)
     return hold_data  # O(1)
